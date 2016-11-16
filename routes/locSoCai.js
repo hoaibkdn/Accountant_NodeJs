d7506789feaeeccba62126aec0959e2f3e06d8c9
Phat Truong Thanh <phat.truong@htklabs.com>
2016-11-16 21:15:54 +0700

    Show tai khoan

--- routes/locSoCai.js
+++ routes/locSoCai.js
@@ -1,2 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var taikhoan = require('../models/taikhoan');
+router.get('/hoaitruong', function(req, res, next){
+  taikhoan.find(function(err,resData){
+    if (err) return err;
+    if(!resData) return null;
+    return res.json({ optionNum :resData});
+  })
+});
+
+module.exports = router;
